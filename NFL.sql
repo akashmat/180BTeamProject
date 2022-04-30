faaa0297f55b8f1a6f67fa9a63c40b40a8af3d2b
MagicSike <linginley@gamil.com>
2022-04-30 10:21:56 -0700

    insert/update/delete DB entries for Admin

--- NFL.sql
+++ NFL.sql
@@ -33,7 +33,7 @@ CREATE TABLE TEAM_MEMBER (
   te_name VARCHAR(20),
 
   PRIMARY KEY (member_id),
-  FOREIGN KEY (te_name) REFERENCES TEAM(team_name) ON DELETE SET NULL
+  FOREIGN KEY (te_name) REFERENCES TEAM(team_name) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE PLAYER (
@@ -65,7 +65,7 @@ CREATE TABLE PLAYER_SCORE (
   total_fumbles INTEGER DEFAULT 0,
   number_games_played INTEGER DEFAULT 0,
 
-  PRIMARY KEY (p_year),
+  PRIMARY KEY (p_year, pscore_id), 
   FOREIGN KEY (pscore_id) REFERENCES TEAM_MEMBER(member_id) ON DELETE CASCADE
 );
 
@@ -79,7 +79,7 @@ CREATE TABLE COACH_SCORE (
   conference_champ_won INTEGER DEFAULT 0,
   division_champ_won INTEGER DEFAULT 0,
 
-  PRIMARY KEY (c_year),
+  PRIMARY KEY (c_year, cscore_id),
   FOREIGN KEY (cscore_id) REFERENCES TEAM_MEMBER(member_id) ON DELETE CASCADE
 );
 
@@ -135,7 +135,7 @@ CREATE TABLE FAN (
   CONSTRAINT unique_fan_team CHECK (Fa_team <> Fo_team),
   FOREIGN KEY (Fa_team) REFERENCES TEAM(team_name), -- no need to cascade since teams should not be deleted
   FOREIGN KEY (Fo_team) REFERENCES TEAM(team_name), -- also, the constraint is check that no  two teams will ever be the same
-  FOREIGN KEY (admin_ver_name) REFERENCES ADMINISTRATOR(username) ON DELETE SET NULL,
+  FOREIGN KEY (admin_ver_name) REFERENCES ADMINISTRATOR(username) ON DELETE SET NULL ON UPDATE CASCADE,
   UNIQUE (username, email, hashed_password)
 );
 
@@ -144,8 +144,8 @@ CREATE TABLE NOTIFIES (
   p_id INTEGER NOT NULL,
   
   PRIMARY KEY (m_id, p_id),
-  FOREIGN KEY (m_id) REFERENCES [MATCH](match_id) ON DELETE CASCADE,
-  FOREIGN KEY (p_id) REFERENCES FAN(profile_id) ON DELETE CASCADE
+  FOREIGN KEY (m_id) REFERENCES [MATCH](match_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (p_id) REFERENCES FAN(profile_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE POLLS (
@@ -155,10 +155,12 @@ CREATE TABLE POLLS (
   creation_date date NOT NULL, 
   poll_name VARCHAR(20) NOT NULL,
   poll_percentage VARCHAR(7), --10%-90%
-  number_of_predictions INTEGER NOT NULL
+  number_of_predictions INTEGER NOT NULL,
+  team_name1 VARCHAR(20) NOT NULL,
+  team_name2 VARCHAR(20) NOT NULL,
 
   PRIMARY KEY (poll_id),
-  FOREIGN KEY (creator_name) REFERENCES ADMINISTRATOR(username) ON DELETE CASCADE,
+  FOREIGN KEY (creator_name) REFERENCES ADMINISTRATOR(username) ON DELETE CASCADE ON UPDATE CASCADE,
 
 );
 
@@ -181,19 +183,35 @@ CREATE TABLE INTERACTS (
 );
 GO
 
-INSERT INTO TEAM VALUES('team_name1', 'mascot1', 'city1', 'logo1', 'home_stad1', 'div1', 'AFC', 'owner1', 'blue', 23, '2000-01-27');
-INSERT INTO TEAM VALUES('team_name2', 'mascot2', 'city2', 'logo2', 'home_stad2', 'div2', 'AFC', 'owner2', 'blue2', 23, '1999-01-26');
+INSERT INTO TEAM VALUES('team_name1', 'mascot1', 'city1', 'logo1', 'home_stad1', 'div1', 'AFC', 'owner1', 'blue1', 23, '2000-01-27');
+INSERT INTO TEAM VALUES('team_name2', 'mascot2', 'city2', 'logo2', 'home_stad2', 'div1', 'AFC', 'owner2', 'blue2', 23, '1999-01-26');
+INSERT INTO TEAM VALUES('team_name3', 'mascot3', 'city3', 'logo3', 'home_stad3', 'div1', 'AFC', 'owner3', 'blue3', 23, '2001-01-27');
+INSERT INTO TEAM VALUES('team_name4', 'mascot4', 'city4', 'logo4', 'home_stad4', 'div1', 'AFC', 'owner4', 'blue4', 23, '2005-01-26');
+INSERT INTO TEAM VALUES('team_name5', 'mascot5', 'city5', 'logo5', 'home_stad5', 'div2', 'AFC', 'owner5', 'blue5', 23, '2002-01-27');
+INSERT INTO TEAM VALUES('team_name6', 'mascot6', 'city6', 'logo6', 'home_stad6', 'div2', 'AFC', 'owner6', 'blue6', 23, '1997-01-26');
+INSERT INTO TEAM VALUES('team_name7', 'mascot7', 'city7', 'logo7', 'home_stad7', 'div2', 'AFC', 'owner7', 'blue7', 23, '2000-01-27');
+INSERT INTO TEAM VALUES('team_name8', 'mascot8', 'city8', 'logo8', 'home_stad8', 'div2', 'AFC', 'owner8', 'blue8', 23, '1999-01-26');
+INSERT INTO TEAM VALUES('team_name9', 'mascot9', 'city9', 'logo9', 'home_stad9', 'div3', 'NFC', 'owner9', 'blue9', 23, '1996-01-27');
+INSERT INTO TEAM VALUES('team_name10', 'mascot10', 'city10', 'logo10', 'home_stad10', 'div3', 'NFC', 'owner10', 'blue10', 23, '1999-01-26');
+INSERT INTO TEAM VALUES('team_name11', 'mascot11', 'city11', 'logo11', 'home_stad11', 'div3', 'NFC', 'owner11', 'blue11', 23, '2010-01-27');
+INSERT INTO TEAM VALUES('team_name12', 'mascot12', 'city12', 'logo12', 'home_stad12', 'div3', 'NFC', 'owner12', 'blue12', 23, '1999-01-26');
+INSERT INTO TEAM VALUES('team_name13', 'mascot13', 'city13', 'logo13', 'home_stad13', 'div4', 'NFC', 'owner13', 'blu13', 23, '1980-01-27');
+INSERT INTO TEAM VALUES('team_name14', 'mascot14', 'city14', 'logo14', 'home_stad14', 'div4', 'NFC', 'owner14', 'blue14', 23, '1999-01-26');
+INSERT INTO TEAM VALUES('team_name15', 'mascot15', 'city15', 'logo15', 'home_stad15', 'div4', 'NFC', 'owner15', 'blue15', 23, '1985-01-27');
+INSERT INTO TEAM VALUES('team_name16', 'mascot16', 'city16', 'logo16', 'home_stad16', 'div4', 'NFC', 'owner16', 'blue16', 23, '1995-01-26');
 
 INSERT INTO TEAM_MEMBER VALUES(1, 'fname1', 'm1', 'lname1', 'team_name1');
 INSERT INTO TEAM_MEMBER VALUES(2, 'fname2', 'm2', 'lname2', 'team_name2');
 INSERT INTO TEAM_MEMBER VALUES(3, 'fname3', 'm3', 'lname3', 'team_name1');
 INSERT INTO TEAM_MEMBER VALUES(4, 'fname4', 'm4', 'lname4', 'team_name1');
+INSERT INTO TEAM_MEMBER VALUES(5, 'fname5', 'm5', 'lname5', null);
 
 INSERT INTO PLAYER VALUES(1);
 INSERT INTO PLAYER VALUES(2);
 
 INSERT INTO COACH VALUES(3);
 INSERT INTO COACH VALUES(4);
+INSERT INTO COACH VALUES(5)
 
 INSERT INTO MEMBER_CONTRACT VALUES(1, 'contract1', '2000-01-20', 'active');
 INSERT INTO MEMBER_CONTRACT VALUES(2, 'contract2', '2000-01-15', 'active');
@@ -201,7 +219,9 @@ INSERT INTO MEMBER_CONTRACT VALUES(3, 'contract3', '2000-01-10', 'active');
 INSERT INTO MEMBER_CONTRACT VALUES(4, 'contract4', '2000-02-10', 'inactive');
 
 INSERT INTO PLAYER_SCORE VALUES(1999, 1, 1, 2, 3, 4, 5);
+INSERT INTO PLAYER_SCORE VALUES(1998, 1, 1, 2, 3, 4, 5);
 INSERT INTO PLAYER_SCORE VALUES(1998, 2, 1, 2, 3, 4, 5);
+
 
 INSERT INTO COACH_SCORE VALUES(1997, 3, 1, 2, 3, 4, 5, 6);
 INSERT INTO COACH_SCORE (c_year, cscore_id) VALUES(1995, 4);
@@ -211,14 +231,16 @@ INSERT INTO TEAM_SCORE VALUES(2000, 'team_name1', 1, 2, 3, 4, 5, 6);
 INSERT INTO TEAM_SCORE VALUES(2001, 'team_name2', 1, 2, 3, 4, 5, 6);
 
 INSERT INTO [MATCH] VALUES(1234, 'team_name1', 'team_name2', '2000-06-01', '6-1', 'venue1', '60%-40%', '40%-60%');
+--INSERT INTO [MATCH] VALUES(4567, 'team_name1', 'team_name1', '2000-06-01', '6-1', 'venue1', '60%-40%', '40%-60%');
 
-INSERT INTO ADMINISTRATOR VALUES('user1', 'email1', 'pass1');
+
+INSERT INTO ADMINISTRATOR VALUES('user1', 'a@blog.com', 'pass');
 
 INSERT INTO FAN VALUES(56, 'user1', 'team_name1', 'team_name2', 'fuser1', 'femail1', 'fpass1');
 
 INSERT INTO NOTIFIES VALUES(1234, 56);
 
-INSERT INTO POLLS VALUES(78, 'user1', 3600, '2000-02-28', 'poll1', '10%-90%', 50);
+INSERT INTO POLLS VALUES(78, 'user1', 3600, '2000-02-28', 'poll1', '10%-90%', 50, 'team_name11', 'team_name22');
 
 INSERT INTO PETITIONS VALUES(56, 'user1');
 
--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+from tabnanny import check
+from flask import Flask, render_template, url_for, flash, redirect, request, session
 from forms import RegistrationForm, LoginForm
-import db_operations as dbOp
+from admin import admin
+#from datetime import date
+#import db_operations as dbOp
+#import random
 #from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 #mysql = MySQL(app)
+app.register_blueprint(admin, url_prefix="")
 
-
-#Testing: Remove posts.
+# Testing: Remove posts.
 posts = [
     {
         'author': 'AM 1',
@@ -24,8 +28,9 @@ posts = [
     }
 ]
 
+
 @app.route("/")
-    
+
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
@@ -34,6 +39,7 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -50,52 +56,10 @@ def login():
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
             flash('You have been logged in!', 'success')
-            return redirect(url_for('homeAdmin'))
-        elif form.email.data == 'user@blog.com' and form.password.data == 'user_password':
-            flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-#Database operations by Administer
-@app.route("/homeAdmin")
-def homeAdmin():
-    return render_template('homeAdmin.html', posts=posts)
-
-@app.route("/readDB", methods=['POST', 'GET'])
-def readDB():
-    try:
-        if request.method == "POST":
-            name = request.form['read']
-            df = dbOp.read_sql("select * from " + name, 0)
-            flash('Read successful!', 'success')
-            return render_template("homeAdmin.html", tables=[df.to_html(classes='data', header="true")])
-        else:
-            return render_template("homeAdmin.html")
-    except:
-        return render_template("homeAdmin.html")
-
-@app.route("/insertDB", methods=['POST', 'GET'])
-def insertDB():
-    try:
-        if request.method == "POST":
-            relation = request.form['relation']
-            values = request.form.getlist('values')
-
-            opDB = "INSERT INTO " + str(relation) + " "
-            str_values = ""
-            for x in values:
-                str_values += str(x)
-            opDB += "VALUES(" + str_values + ")"
-            #print(opDB)
-            dbOp.read_sql(opDB, 1)
-            #flash('Write Success!', 'success')
-            return render_template("homeAdmin.html")
-        else:
-            return render_template("homeAdmin.html")
-    except:
-        return render_template("homeAdmin.html")    
 
 
 if __name__ == '__main__':
